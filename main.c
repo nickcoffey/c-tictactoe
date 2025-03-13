066a54fbf8b17ca5835ba04c3e63f43144f70117
Nicholas Coffey <nick@nicholass-air.lan>
2025-03-12 22:48:29 -0500

    added input validation

--- main.c
+++ main.c
@@ -1,17 +1,35 @@
 #include <stdio.h>
 
-int main() {
-  printf("Welcome to TicTacToe!\n");
-  printf("Moves are made by entering the number that matches like to take.\n");
+void drawBoard() {
   printf("1 | 2 | 3\n");
   printf("4 | 5 | 6\n");
   printf("7 | 8 | 9\n");
+}
 
+int getSelection() {
+  int selection;
+  int isValidInput = 0;
 
-  int place;
-  printf("Enter your selection (1-9): ");
+  do {
+    printf("Enter your selection (1-9): ");
+    scanf("%d", &selection);
+    if (selection < 9 && selection > 1) {
+      isValidInput = 1;
+    } else {
+      printf("Invalid input\n");
+    }
+  } while (!isValidInput);
 
-  scanf("%d", &place);
-  printf("You selected: %d\n", place);
+  return selection;
+}
+
+int main() {
+  printf("Welcome to TicTacToe!\n");
+  printf("Moves are made by entering the number that matches like to take.\n");
+  drawBoard();
+
+  int selection = getSelection();
+
+  printf("You selected: %d\n", selection);
   return 0;
 }
